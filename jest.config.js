62d9d9588d7ce7fb7082afe3285b014884bfd92c
Peter Aiken <pwaiken2@gmail.com>
2022-03-23 18:49:01 +0000

    Moving coverage reports to doc

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
+  coverageDirectory: "docs/coverage",
   preset: 'ts-jest',
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
