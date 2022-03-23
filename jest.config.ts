c1c48ca0995e49357783c449d175233b2d61f108
Peter Aiken <pwaiken2@gmail.com>
2022-03-23 15:18:55 +0000

    Adding test frameworks

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  }
+};
