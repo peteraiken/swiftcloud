261329588cdc4858909b5245dbd2b84baa669806
Peter Aiken <pwaiken2@gmail.com>
2022-03-23 11:35:01 +0000

    Class builder for Song

---
+++ src/parser/base.builder.ts
@@ -0,0 +1,26 @@
+export default abstract class BaseBuilder<T> {
+
+    initialise(header: Array<string>, row: Array<string>): T {
+        throw new Error(`Class Builder not implemented.`);
+    }
+
+    /**
+     * Finds index of `column` provided in `header` array, then finds the value at that index in the `row` array.
+     * Designed to map row values against appropriate column (and property) names.
+     * 
+     * @param header - The array of column names.
+     * @param row - The array of values per column.
+     * @param column - The desired `column` name to find from the `header` array.
+     * @returns String at index of desired `column` in `header` array, as found in the `row` array.
+     */
+    protected getValueByColumnName(header: Array<string>, row: Array<string>, column: string): string | null {
+        // If the requested column is not in the list of columns, return null.
+        if (!header.includes(column)) return null;
+
+        const columnIndex = header.indexOf(column);
+        const value = row[columnIndex];
+        if (!value) return null;
+
+        return value;
+    }
+}
---
+++ src/parser/song.builder.ts
@@ -0,0 +1,42 @@
+import { MonthlyPlays } from "../model/song-monthly-plays.model";
+import { Song } from "../model/song.model";
+import BaseBuilder from "./base.builder";
+
+export default class SongBuilder extends BaseBuilder<Song> {
+
+    /**
+     * Parse string array format from Google Sheets into valid Song object.
+     * 
+     * @param header - The string array of column names.
+     * @param row - The string array of row values.
+     * @returns New song object.
+     */
+    initialise(header: Array<string>, row: Array<string>): Song {
+        if (header.length === 0) throw new Error(`Column header values must be provided.`);
+
+        // Direct mapping.
+        const title = this.getValueByColumnName(header, row, 'Song');
+        const album = this.getValueByColumnName(header, row, 'Album');
+        
+        // Splitting multi-line values into string arrays.
+        const artists = this.getValueByColumnName(header, row, 'Artist')?.split('\n') || null;
+        const writers = this.getValueByColumnName(header, row, 'Writer')?.split('\n') || null;
+        const year = parseInt(this.getValueByColumnName(header, row, 'Year')) || null;
+
+        // Splitting values into numeric plays-per-month.
+        const plays: MonthlyPlays = {
+            june: parseInt(this.getValueByColumnName(header, row, 'Plays - June')) || null,
+            july: parseInt(this.getValueByColumnName(header, row, 'Plays - July')) || null,
+            august: parseInt(this.getValueByColumnName(header, row, 'Plays - August')) || null
+        }
+
+        return {
+            title: title,
+            artists: artists,
+            writers: writers,
+            album: album,
+            year: year,
+            plays: plays
+        }
+    }
+}
